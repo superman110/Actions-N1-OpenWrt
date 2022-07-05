6b2f74d50bbfb8ee114877e9e185139b852bdd06
superman110 <superman110@gmail.com>
2022-07-06 04:09:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -77,7 +77,7 @@ svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-amlogic packag
 #修改晶晨宝盒默认配置
 
 # 1.Set the download repository of the OpenWrt files to your github.com
-sed -i "s|https.*/OpenWrt|https://github.com/superman110/Docker-Openwrt-N1|g" package/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|https.*/OpenWrt|https://github.com/superman110/Actions-N1-OpenWrt|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 # 2.Set the keywords of Tags in your github.com Releases
 #sed -i "s|ARMv8|ARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
